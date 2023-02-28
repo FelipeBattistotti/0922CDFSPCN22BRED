ca537a74b90a0df21b8cfe9962806888da943606
FelipeBattistotti <felipe.battistotti@gmail.com>
2023-02-27 21:03:22 -0300

    [UPDATE] fake-insta

--- aulas/js-front/fake-insta/js/index.js
+++ aulas/js-front/fake-insta/js/index.js
@@ -1,10 +1,31 @@
-console.log('INSTA')
+const mouseOverButton = () => {
+  console.log('MOUSE OVER BUTTON !')
+}
 
-window.addEventListener('load', () => {
-
+window.onload = function() {
   // querySelector
-  // const button = document.querySelector('button')
-  // button.innerText = 'Cadastre-se'
+  const button = document.querySelector('button')
+  button.innerText = 'Cadastre-se'
+
+  // 1
+  // button.onmouseover = mouseOverButton
+
+  // 2
+  // button.onmouseover = () => {
+  //   console.log('MOUSE OVER BUTTON !!!')
+  // }
+
+  // 3
+  button.addEventListener('mouseover', function() {
+    button.style.cursor = 'pointer'
+    button.style.backgroundColor = '#0077c4'
+  })
+  button.addEventListener('mouseout', function() {
+    button.style.cursor = 'auto'
+    button.style.backgroundColor = '#0095f6'
+  })
+
+
 
   // getElementsByTagName
   // const button = document.getElementsByTagName('button')
@@ -19,8 +40,8 @@ window.addEventListener('load', () => {
   userName.innerHTML = '<strong>Logado</strong>'
 
   // querySelector
-  const button = document.querySelector('div .button')
-  button.innerText = 'Por favor cadastre-se'
+  // const button = document.querySelector('div .button')
+  // button.innerText = 'Por favor cadastre-se'
 
   // getElementById
   const connect = document.getElementById('connect')
@@ -39,5 +60,46 @@ window.addEventListener('load', () => {
   const register = document.querySelector('.register')
   if (register.classList.contains('register'))
       register.classList.remove('register')
+}
 
-})
+// window.addEventListener('load', () => {
+
+//   // querySelector
+//   // const button = document.querySelector('button')
+//   // button.innerText = 'Cadastre-se'
+
+//   // getElementsByTagName
+//   // const button = document.getElementsByTagName('button')
+//   // button[0].innerText = 'Cadastre-se'
+
+//   // querySelector
+//   // const userName = document.querySelector('.avatar-content div')
+//   // userName.innerHTML = '<strong>Nome Usuário</strong>'
+
+//   // querySelector
+//   const userName = document.querySelector('.avatar-content p')
+//   userName.innerHTML = '<strong>Logado</strong>'
+
+//   // querySelector
+//   const button = document.querySelector('div .button')
+//   button.innerText = 'Por favor cadastre-se'
+
+//   // getElementById
+//   const connect = document.getElementById('connect')
+//   connect.innerText += ' aqui'
+
+//   // querySelectorAll
+//   const inputs = document.querySelectorAll('.input')
+//   inputs.forEach(element => {
+//     // style
+//     element.style.borderRadius = '4px'
+//     // classList
+//     element.classList.add('active')
+//   })
+
+//   // querySelector
+//   const register = document.querySelector('.register')
+//   if (register.classList.contains('register'))
+//       register.classList.remove('register')
+
+// })
