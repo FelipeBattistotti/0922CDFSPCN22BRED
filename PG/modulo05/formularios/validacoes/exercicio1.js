0533e368887b48da1e6acffb623116a8d1323287
FelipeBattistotti <felipe.battistotti@gmail.com>
2023-03-02 21:01:39 -0300

    [ADD] Javascript Front => Formulários => Validações => Exercício I

---
+++ PG/modulo05/formularios/validacoes/exercicio1.js
@@ -0,0 +1,14 @@
+window.addEventListener('load', () => {
+  const formulario = document.getElementById('formulario')
+  const emailInput = document.getElementById('email')
+  const senhaInput = document.getElementById('password')
+  const msgSucesso = document.getElementById('msg-sucesso')
+  
+  formulario.addEventListener('submit', (e) => {
+      e.preventDefault()
+
+      if (emailInput.value.length > 0 && senhaInput.value.length > 0) {
+          msgSucesso.style.display = 'block'
+      }
+  })
+})
