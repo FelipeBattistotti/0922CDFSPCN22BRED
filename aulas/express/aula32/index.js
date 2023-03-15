4b78cf0dd813bab682a6fece4557dda6b698eadb
FelipeBattistotti <felipe.battistotti@gmail.com>
2023-03-15 20:33:32 -0300

    Aula 33

--- aulas/express/aula32/index.js
+++ aulas/express/aula32/index.js
@@ -12,10 +12,39 @@ app.get('/pessoa', (req, res) => {
   res.json(pessoas)
 })
 
+// # 1
 app.get('/pessoa/:id', (req, res) => {
   const { id } = req.params
-  console.log('ID: ', id)
-  res.send('ACHOU')
+  
+  const pessoa = pessoas.find(pessoa => String(pessoa.id) === id)
+
+  if (pessoa)
+      return res.json(pessoa)
+  else return res.status(400).json({ error: 'Usuário não encontrado.' })
+})
+// # 2
+app.put('/pessoa/:id', (req, res) => {
+  const { id } = req.params
+  
+  const pessoaIndex = pessoas.findIndex(pessoa => String(pessoa.id) === id)
+
+  if (pessoaIndex != -1) {
+      pessoas[pessoaIndex] = req.body
+      return res.json(pessoas)
+  }
+  else return res.status(400).json({ error: 'Usuário não encontrado.' })
+})
+// # 3
+app.delete('/pessoa/:id', (req, res) => {
+  const { id } = req.params
+  
+  const pessoaIndex = pessoas.findIndex(pessoa => String(pessoa.id) === id)
+
+  if (pessoaIndex != -1) {
+      pessoas.splice(pessoaIndex, 1)
+      return res.json(pessoas)
+  }
+  else return res.status(400).json({ error: 'Usuário não encontrado.' })
 })
 
 app.post('/pessoa', (req, res) => {
