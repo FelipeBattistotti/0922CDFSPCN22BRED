1cac40656bbe3fd70e01eb0c3fcc57a2a461b764
FelipeBattistotti <felipe.battistotti@gmail.com>
2023-03-22 20:40:54 -0300

    [UPDATE] PI Express

--- pi-express/src/app.js
+++ pi-express/src/app.js
@@ -1,11 +1,14 @@
 const routes = require('./routes/index')
 const path = require("path")
 const express = require('express')
+const methodOverride = require('method-override') // métodos PUT e DELETE
 const app = express()
 // captura na forma de objeto literal tudo o que vem de um formulário
 app.use(express.urlencoded({ extended: false }))
 // converte as informações em formato JSON
 app.use(express.json())
+// métodos PUT e DELETE
+app.use(methodOverride('_method'))
 
 // instanciando como view engine
 app.set("view engine", "ejs")
