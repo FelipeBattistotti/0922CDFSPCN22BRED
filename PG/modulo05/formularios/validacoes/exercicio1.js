475b8bfec2fd30d047c6b01ca3eb8e02b5a79e5b
FelipeBattistotti <felipe.battistotti@gmail.com>
2023-03-24 21:03:15 -0300

    Módulo 05 - Exercício I - Formulários - Validações

--- PG/modulo05/formularios/validacoes/exercicio1.js
+++ PG/modulo05/formularios/validacoes/exercicio1.js
@@ -3,12 +3,12 @@ window.addEventListener('load', () => {
   const emailInput = document.getElementById('email')
   const senhaInput = document.getElementById('password')
   const msgSucesso = document.getElementById('msg-sucesso')
-  
-  formulario.addEventListener('submit', (e) => {
-      e.preventDefault()
 
-      if (emailInput.value.length > 0 && senhaInput.value.length > 0) {
-          msgSucesso.style.display = 'block'
-      }
+  formulario.addEventListener('submit', (e) => {
+    e.preventDefault()
+
+    if (emailInput.value.length > 0 && senhaInput.value.length > 0) {
+        msgSucesso.style.display = 'block'
+    }
   })
 })
