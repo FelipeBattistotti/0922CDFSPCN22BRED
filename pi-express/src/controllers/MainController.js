4834d52c461720dc991cf394430457b17115088a
FelipeBattistotti <felipe.battistotti@gmail.com>
2023-04-26 21:55:05 -0300

    findAll() - Sequelize

--- pi-express/src/controllers/MainController.js
+++ pi-express/src/controllers/MainController.js
@@ -1,13 +1,19 @@
-const products = require('../database/products.json')
+const { Product } = require('../models')
 
 const toThousand = n => n.toString().replace(/\B(?=(\d{3})+(?!\d))/g, ".")
 
 const MainController = {
-  index: (req, res) => {
-    res.render('index', {
-      products,
-      toThousand
-    })
+  index: async (req, res) => {
+    try {
+      const products = await Product.findAll()
+
+      res.render('index', {
+        products,
+        toThousand
+      })
+    } catch (error) {
+      res.status(400).json({ error })
+    }
   },
   search: (req, res) => {
     let search = req.query.keywords
