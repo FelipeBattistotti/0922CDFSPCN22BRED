ff4f83662498dc5dac47b4440b03ecdbfc3cb83e
FelipeBattistotti <felipe.battistotti@gmail.com>
2023-05-24 20:50:26 -0300

    [UPDATE] Pesquisa de Produtos

--- pi-backend/src/controllers/MainController.js
+++ pi-backend/src/controllers/MainController.js
@@ -1,23 +1,15 @@
 const { Op } = require('sequelize')
 const { Product } = require('../models')
 
-const toThousand = n => n.toString().replace(/\B(?=(\d{3})+(?!\d))/g, ".")
-
 const MainController = {
   index: async (req, res) => {
-    try {
-      const products = await Product.findAll()
+    let search = ''
 
-      res.status(200).json(products)
-    } catch (error) {
-      res.status(400).json({ error })
-    }
-  },
-  search: async (req, res) => {
-    let search = req.query.keywords
+    if (req.query.keywords)
+        search = req.query.keywords
 
     try {
-      const productsToSearch = await Product.findAll({
+      const products = await Product.findAll({
         where: {
           name: {
             [Op.substring]: search
@@ -25,11 +17,7 @@ const MainController = {
         }
       })
 
-      res.render('results', {
-        products: productsToSearch,
-        search,
-        toThousand,
-      })
+      res.status(200).json(products)
     } catch (error) {
       res.status(400).json({ error })
     }
