a3ea2f58e4a6b2d5bc0577297504e8b2b7e638be
FelipeBattistotti <felipe.battistotti@gmail.com>
2023-05-29 22:15:20 -0300

    Commit inicial - React

---
+++ pi-frontend/src/App.jsx
@@ -0,0 +1,14 @@
+import React from 'react';
+import './App.css';
+import Header from './components/Header';
+
+function App() {
+  return (
+    // JXS
+    <div className="App">
+      <Header />
+    </div>
+  );
+}
+
+export default App;
